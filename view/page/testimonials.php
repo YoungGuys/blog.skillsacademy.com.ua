6b9d5db781e3f0d44c405628dd40e46c9879043e
Коля <kolka.koval@gmail.com>
2015-02-01 17:34:14 +0200

    style

---
+++ view/page/testimonials.php
@@ -0,0 +1,214 @@
+<div class="testimonials">
+
+    <div class="expert">
+        <div class="ex-slider">
+            <div class="ex-slider__content">
+                <h2 class="expert__title">experts&users</h2>
+                <div class="ex-slider__content__item">
+                    <span class="ex-slider__experts">Liudmila Bogush-Dand, BogushTime</span>
+                    <span class="ex-slider__users">CEO, couch</span>
+                    <p class="ex-slider__text">
+                        “As a designer, you’re constantly pulled in a dozen different directions.
+                        Every phase of a project requires a different skillset and level of focus.
+                        So to get the job done, you need to grow your skills—and find and use”
+                    </p>
+                </div>
+                <div class="ex-slider__content__item">
+                    <span class="ex-slider__experts">Liudmila Bogush-Dand, BogushTime CEO, couch</span>
+                    <span class="ex-slider__users">Liudmila Bogush-Dand, BogushTime CEO, couch</span>
+                    <p class="ex-slider__text">
+                        “As a designer, you’re constantly pulled in a dozen different directions.
+                        Every phase of a project requires a different skillset and level of focus.
+                        So to get the job done, you need to grow your skills—and find and use”
+                    </p>
+                </div>
+            </div>
+            <div class="ex-slider__pic">
+                <div class="ex-slider__pic__item">
+                    <img src="http://placekitten.com/560/360" alt="slide-pic"/>
+                </div>
+                <div class="ex-slider__pic__item">
+                    <img src="http://placekitten.com/560/360" alt="slide-pic"/>
+                </div>
+            </div>
+            <div class="ex-slider__control">
+                <i class="btn-slider__control ex-slider__control--prev js-btn_prev_slide"></i>
+                <i class="btn-slider__control ex-slider__control--next js-btn_next_slide"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="company">
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+        <div class="company__item">
+            <img class="company__item__pic" src="http://placekitten.com/110/90" alt="company-logo"/>
+        </div>
+    </div>
+
+    <div class="users-say">
+        <h2 class="users-say__title">WHAT THE USERS SAY?</h2>
+
+        <div class="users-say__content">
+            <div class="users-say__content__col">
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        As a designer, you’re constantly pulled in a dozen different
+                        directions. Every phase of a project requires a different
+                        skillset and level of focus.
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto aspernatur commodi, hic iusto modi molestiae numquam quidem quis.
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis
+                        delectus expedita fugit maiores quaerat reiciendis sequi! Dicta magni nisi sapiente? Ad animi ex sint!
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="users-say__content__col">
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid atque beatae consequatur debitis delectus, dolores eos eveniet exercitationem harum nisi, nostrum numquam odio perspiciatis, porro similique tenetur voluptate voluptatum.
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, maiores neque nisi obcaecati possimus sint voluptatibus! Ea esse ipsa obcaecati, odio quidem rem totam?
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="users-say__content__col">
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, labore.
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cupiditate dolores harum impedit nihil!
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+                <div class="users-say__content__item">
+                    <h6 class="users-say__content__item__title">Support is brilliant</h6>
+
+                    <p class="users-say__content__item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto et eum fuga ipsa, numquam reiciendis sunt tempore?
+                    </p>
+
+                    <div class="users-say__content__item__info">
+                        <img class="users-say__content__item__info__pic" src="http://placekitten.com/90/91" alt="user-avatar"/>
+                        <span class="users-say__content__item__info__name">Lil Wayne</span>
+                        <span class="users-say__content__item__info__job">Shopify</span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+</div>
