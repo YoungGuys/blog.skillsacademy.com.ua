aa975cc1ee58e85bf3501fedfa1abf86fc4dcec2
d1mdem <d1m-dem@rambler.ru>
2015-02-02 17:50:28 +0200

    first commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         require ('core/lib/php/autoloader.php');
         require ('config.php');
         define("DEV_MOD",true);
-
+// Dima was here
         $rout = new \Balon\Routing();
         $rout->loadPage();
 
